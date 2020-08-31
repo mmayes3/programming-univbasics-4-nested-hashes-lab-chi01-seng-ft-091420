5a558031f723abd13e10af2e40a8376db1077500
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-31 01:18:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,11 +15,12 @@ def hopper
         :languages => ["C"]
       }
     }
+    return programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
 	
 	programmer_hash = 
  		{
@@ -36,6 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    return programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    return programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
